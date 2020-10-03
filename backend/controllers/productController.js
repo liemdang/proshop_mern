094ba702b43fb6d7f5db4425a1be03ec42faeeb5
liem <liem.dang@hotmail.com>
2020-10-03 19:01:12 +0200

    user authentication endpoint

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,16 +1,16 @@
 import asyncHandler from "express-async-handler";
 import Product from "../models/productModel.js";
 
-// @desc    Fetch single product
-// @route   GET /api/products/:id
+// @desc    Fetch all products
+// @route   GET /api/products
 // @acess   Public
 const getProducts = asyncHandler(async (req, res) => {
   const products = await Product.find({});
   res.json(products);
 });
 
-// @desc    Fetch all products
-// @route   GET /api/products
+// @desc    Fetch single product
+// @route   GET /api/products/:id
 // @acess   Public
 const getProductById = asyncHandler(async (req, res) => {
   const product = await Product.findById(req.params.id);
